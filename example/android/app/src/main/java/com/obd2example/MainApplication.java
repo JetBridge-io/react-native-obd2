d9dbfece053551c09ef167314d0fc1748e6a2116
Brandon Seo <Brandon.Seo@jetbridge.io>
2017-04-11 13:00:31 +0900

    Duplicated import
    Removed duplicated import

--- example/android/app/src/main/java/com/obd2example/MainApplication.java
+++ example/android/app/src/main/java/com/obd2example/MainApplication.java
@@ -4,13 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.jetbridge.reactobd2.ReactNativeOBD2Package;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.jetbridge.reactobd2.ReactNativeOBD2Package;
 import com.zenome.rnsp.RnspReactPackage;
 import com.sensormanager.SensorManagerPackage;
+import com.jetbridge.reactobd2.ReactNativeOBD2Package;
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,12 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeOBD2Package(),
-            new VectorIconsPackage(),
-            new RnspReactPackage(),
-            new SensorManagerPackage(),
-            new ReactNativeDialogsPackage(),
-            new ReactNativeOBD2Package()
+          new RnspReactPackage(),
+          new SensorManagerPackage(),
+          new ReactNativeDialogsPackage(),
+          new VectorIconsPackage(),
+          new ReactNativeOBD2Package()
       );
     }
   };
